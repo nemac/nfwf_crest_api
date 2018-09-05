bdd9dcdc0cc4517f83080d2b3f5d3eadf54ba958
Matthew Geiger <mgeiger@unca.edu>
2018-09-05 13:43:05 -0400

    More tinkering for travis...

--- run-tests.sh
+++ run-tests.sh
@@ -4,15 +4,6 @@ set -ev
 
 source '.env'
 
-funcs=( 'identify_function' 'zonal_stats_function' 'upload_shape_function' )
-
-for i in "${funcs[@]}"
-do
-	cp $CONFIG_FILE_DEV $CONFIG_FILE_PROD $i/
-	cp $VRT_FILE_DEV $VRT_FILE_PROD $i/
-	cp -R lib/ $i/
-done
-
 pip install -U pip pipenv
 pipenv install --dev
 
