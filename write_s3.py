4f41e83040f4cae55f3364d4017b766df3caa057
Jeff Bliss <jeff.a.bliss@gmail.com>
2024-02-29 07:17:12 -0500

    changes needed to get link sharing working

--- write_s3.py
+++ write_s3.py
@@ -2,11 +2,23 @@ import json
 import boto3
 
 def lambda_handler(event, context):
-    s3 = boto3.client('s3')
-    json_data = event['state']
+  s3 = boto3.client('s3')
+  # Parse the JSON body
+  body = json.loads(event['body'])
+  state = body['state']
+  location = body['location']
     
-    s3.put_object(
-        Body=json.dumps(json_data),
-        Bucket='crest-v2-share-link',
-        Key=event['location']
-    )
+  s3.put_object(
+    Body=json.dumps(state).encode(),
+    Bucket='crest-v2-share-link',
+    Key=location
+  )
+
+  return {
+    'statusCode': 200,
+    'body': 'Write to S3 successful: ' + location,
+    "headers": {
+      "Content-Type": '*/*',
+      "Access-Control-Allow-Origin": "*"
+    }
+  }
